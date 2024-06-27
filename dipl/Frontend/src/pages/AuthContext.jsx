d7d89d3705d0173d956e8f068bbce55c5ba2e1b6
Jitendra0564 <132242846+Jitendra0564@users.noreply.github.com>
2024-06-27 17:02:11 +0530

    Update AuthContext.jsx

--- dipl/Frontend/src/pages/AuthContext.jsx
+++ dipl/Frontend/src/pages/AuthContext.jsx
@@ -1,5 +1,5 @@
 // src/contexts/AuthContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext ,useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
