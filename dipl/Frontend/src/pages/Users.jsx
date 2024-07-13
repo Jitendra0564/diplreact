06081ae7b6d0365242d88f506ac214ef15bcffb4
Jitendra0564 <132242846+Jitendra0564@users.noreply.github.com>
2024-07-13 12:51:14 +0530

    Update Users.jsx

--- dipl/Frontend/src/pages/Users.jsx
+++ dipl/Frontend/src/pages/Users.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import Sidebar from '../components/Common/Sidebar';
 import UserTable from '../components/Users/UserTable';
-import Navbar2 from '../components/Common/Navbar2';
 import AddEmployeeForm from '../components/Users/AddEmployeeForm';
-
+import { useNavigate } from 'react-router-dom';
 
 const Users = () => {
     const [showAddEmployeeForm, setShowAddEmployeeForm] = useState(false);
-
+    const navigate = useNavigate();
 
     const handleAddEmployeeClick = () => {
         setShowAddEmployeeForm(true);
@@ -24,7 +23,12 @@ const Users = () => {
                 <Sidebar />
                 <div className='bg-gray-300 relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden'>
                     <main>
-                        <Navbar2 isUserPage={true} onAddEmployeeClick={handleAddEmployeeClick} />
+                      <button
+                onClick={() => navigate("/todo")}
+                className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 p-4 mx-4 my-4"
+              >
+                Back
+              </button>
 
                         {/* Conditionally render the AddEmployeeForm or UserTable based on state */}
                         {showAddEmployeeForm ? (
